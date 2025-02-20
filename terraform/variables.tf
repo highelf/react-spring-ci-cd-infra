59a3f9cc777dc2089c4ab7459056fb445c49530b
Amir Modiri <amir.modiri.master@gmail.com>
2025-02-20 09:09:33 +0100

    deploy terraform and add backend into cloud

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
 variable "environment" {
   type    = string
   default = "dev" # Change to test/prod as needed
